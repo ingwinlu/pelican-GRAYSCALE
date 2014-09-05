36d0c089dedfcc79339fca87e51755d5a381e412
winlu <derwinlu+git@gmail.com>
2014-09-05 21:42:59 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+pelican-single-page
+===================
+
+
+renders a single pelican page, see `sample_pelicanconf.py` for a config which works.
+
---
+++ sample_pelicanconf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'winlu'
+SITENAME = 'pelican single page showoff'
+SITEURL = 'http://single.heroicdebugging.biz'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+
+
+###theme
+THEME = "/home/winlu/gitrepos/pelican_my_themes/pelican-single-page"
+
+DIRECT_TEMPLATES = ('index',)
+PAGE_SAVE_AS = ''
